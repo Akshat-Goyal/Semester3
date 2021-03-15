d60597153cfd50937d6202c334eab28f242b30d3
Akshat-Goyal <45856816+Akshat-Goyal@users.noreply.github.com>
2021-03-15 16:14:31 +0530

    Delete cse/2018101075_Assignment5 directory

--- cse/2018101075_Assignment5/xv6/pinfo.h
+++
@@ -1,9 +0,0 @@
-#ifdef MLFQ
-    struct proc_stat{
-        int pid;
-        long long int runtime;
-        long long int num_run;
-        int current_queue;
-        long long int ticks[NPQ];
-    };
-#endif
